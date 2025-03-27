36c9ff50d7cc3ae78c6f76b98b3acd9120169d4a
Lukas Mirsch <lukas.mirsch@student.uibk.ac.at>
2025-03-27 16:38:05 +0100

    stop

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ let stop = {
     nr: 5,
     title: "Rotorua",
     user: "lukas6020",
-    lat: lat,
-    lng: lng,
-    zoom: zoom
+    lat: -38.14,
+    lng: 176.25,
+    zoom: 13
 };
 
 // Karte initialisieren
-let map = L.map('map').setView([lat, lng], zoom);
+let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -24,15 +24,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Marker zeichnen
-let marker = L.marker([lat, lng]).addTo(map);
+let marker = L.marker([stop.lat, stop.lng]).addTo(map);
 
 
 // Popup definieren und öffnen
 marker.bindPopup(`
-    <h2>Rotorua</h2>
+    <h2>${stop.title}</h2>
     <ul>
-        <li>Geographische Länge: ${lat.toFixed(2)}</li>
-        <li>Geographische Breite: ${lng.toFixed(2)}</li>
+        <li>Geographische Länge: ${stop.lat.toFixed(2)}°</li>
+        <li>Geographische Breite: ${stop.lng.toFixed(2)}°</li>
     </ul>
 `).openPopup();
 
