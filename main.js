0a2e6705cfdfd6271788b2be85f8ea2e854eb62f
Lukas Mirsch <lukas.mirsch@student.uibk.ac.at>
2025-03-27 15:50:06 +0100

    lat usw eingefügt

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 /*
 Skript für die Neuseelandreise
 */
+let lat = -38.14;
+let lng = 176.25;
+let zoom = 13;
 
 // Karte initialisieren
-let map = L.map('map').setView([-38.14, 176.25], 13);
+let map = L.map('map').setView([lat , lng], zoom);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,7 +15,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Marker zeichnen
-let marker = L.marker([-38.14, 176.25]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 
 // Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
